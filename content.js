9a8992b80cfc3c72819ba535b09302459cc3165c
Raahil2909 <raahilbadiani@gmail.com>
2022-09-09 17:52:55 +0530

    sent message from content to scriptjs

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
 console.log('[+] content.js');
+
+chrome.runtime.sendMessage({
+    message:{url:location.href}
+})
+
+chrome.runtime.onMessage.addListener((request,sender,sendResponse)=>{
+        console.log(request.message.hi)
+    }
+)
