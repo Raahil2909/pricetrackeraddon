c124ea678e2b0cbe993672a0ab104ec477ff73f1
Raahil2909 <raahilbadiani@gmail.com>
2022-10-11 16:23:27 +0530

    refresh button added

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,15 @@
 console.log('[+] script.js');
+
 let targetPageUrl = '';
 let serverUrl = 'http://127.0.0.1:5000/amazon';
+
 let trackBtn = document.querySelector('#track-btn');
 let removeBtns = document.querySelectorAll('.remove-item-btns');
+let refreshBtn = document.querySelector('#refresh-btn');
+
+let refreshIcon = '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="24px" height="24px"><path d="M 15 3 C 12.031398 3 9.3028202 4.0834384 7.2070312 5.875 A 1.0001 1.0001 0 1 0 8.5058594 7.3945312 C 10.25407 5.9000929 12.516602 5 15 5 C 20.19656 5 24.450989 8.9379267 24.951172 14 L 22 14 L 26 20 L 30 14 L 26.949219 14 C 26.437925 7.8516588 21.277839 3 15 3 z M 4 10 L 0 16 L 3.0507812 16 C 3.562075 22.148341 8.7221607 27 15 27 C 17.968602 27 20.69718 25.916562 22.792969 24.125 A 1.0001 1.0001 0 1 0 21.494141 22.605469 C 19.74593 24.099907 17.483398 25 15 25 C 9.80344 25 5.5490109 21.062074 5.0488281 16 L 8 16 L 4 10 z"/></svg>'
 let trashIcon = '<svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 30 30" width="24px" height="24px"><path d="M 13 3 A 1.0001 1.0001 0 0 0 11.986328 4 L 6 4 A 1.0001 1.0001 0 1 0 6 6 L 24 6 A 1.0001 1.0001 0 1 0 24 4 L 18.013672 4 A 1.0001 1.0001 0 0 0 17 3 L 13 3 z M 6 8 L 6 24 C 6 25.105 6.895 26 8 26 L 22 26 C 23.105 26 24 25.105 24 24 L 24 8 L 6 8 z"/></svg>'
 let itemid = 0; // get this from storage
-
-// import  {replacer,reviver} from '../serialize/serializer';
 
 chrome.storage.sync.get(['itemid'],function(res){
     if(res?.itemid) itemid = res.itemid;
@@ -83,6 +86,19 @@ trackBtn.addEventListener("click",async ()=>{
     });
 });
 
+refreshBtn.addEventListener("click",refreshTable);
+
+function refreshTable(){
+    console.log('refreshing....\n');
+    clearTable();
+    fillTable();
+}
+
+function clearTable(){
+    let itemsTableBody = document.querySelector('#items-table-body');
+    itemsTableBody.innerHTML = '';
+}
+
 function addRowInTable(rowdata,rid){
     // <td scope="row">${i}</th>
     let newrow = `<tr id="row-items-${rid}">
@@ -92,8 +108,8 @@ function addRowInTable(rowdata,rid){
                     <td>${rowdata.threshold}</td>
                     <td><button id="${rid}" class="remove-item-btns" style="border:0; background-color:white;">${trashIcon}</button></td>
                   </tr>`
-    let itemsTable = document.querySelector('#items-table-body');
-    itemsTable.innerHTML += newrow;
+    let itemsTableBody = document.querySelector('#items-table-body');
+    itemsTableBody.innerHTML += newrow;
 }
 
 function fillTable(){ 
